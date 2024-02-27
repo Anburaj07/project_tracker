c3b860d74f47f8f3444e6dafdf274000c49ce6b0
Anburaj S <anburaj2203@gmail.com>
2024-02-27 11:56:12 +0530

    created basic express app linked with mongoDB

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT;
+app.use(express.json());
+
+const cors = require("cors");
+const { connection } = require("./db");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Project Tracker Base Point" });
+});
+
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("connected to DB successfully!");
+    console.log(`Server is running at port ${PORT}`);
+  } catch (error) {
+    console.log("Error while connecting DB");
+    console.log("error:", error);
+  }
+});
